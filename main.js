98dddfff637edeaed603723188cdf242360199fa
geof.hoffman <geof.hoffman.nfts@gmail.com>
2021-06-12 11:56:57 -0400

    electron boilerblate

---
+++ main.js
@@ -0,0 +1,16 @@
+const { BrowserWindow, app } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        backgroundColor: "white",
+        nodeIntegration: false,
+        worldSafeExecuteJavaScript: true,
+        contextIsolation: true
+    })
+
+
+win.loadFile('index.html');
+}
+app.whenReady().then(createWindow)
